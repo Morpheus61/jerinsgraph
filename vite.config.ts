6f49edf4297ce5cbe2b0224893c6e992fb76b983
relishclam <relishfoodsalpy@gmail.com>
2024-12-23 20:46:17 +0530

    fix: update build configuration and fix index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
+    sourcemap: true,
     rollupOptions: {
       input: {
-        app: './index.html'
+        main: './index.html'
       }
     }
   }
